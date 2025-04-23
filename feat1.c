b5a2d400d3ca4792aaaea90ca60e1f911c042298
Sanket <sanket@theplasma.tech>
2025-04-23 09:52:52 +0530

    feat: sum in C

--- feat1.c
+++ feat1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int sum(int a, int b){
+    return a++b;
+}
+
 int main(){
-    printf("Feature 1");
+    int a = 56;
+    int b = 78;
+    int sum = sum(a, b);
     return 0;
 }
