9d9546bae95ea861d25945bb25606617ffc05827
Sanket <sanket@theplasma.tech>
2025-04-23 09:57:48 +0530

    Revert "feat: sum in C"
    This reverts commit b5a2d400d3ca4792aaaea90ca60e1f911c042298.

--- feat1.c
+++ feat1.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-int sum(int a, int b){
-    return a++b;
-}
-
 int main(){
-    int a = 56;
-    int b = 78;
-    int sum = sum(a, b);
+    printf("Feature 1");
     return 0;
 }
--- main.c
+++ main.c
@@ -1,6 +1,2 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World!");
-    return 0;
-}
